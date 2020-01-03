88217f76d8c3022edd3f3ff481dd89fdb6f6e0db
himself65 <himself65@outlook.com>
2020-01-03 18:21:01 +0800

    feat: update project using 'rollup-plugin-chrome-extension'

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+
+import { chromeExtension, pushReloader } from 'rollup-plugin-chrome-extension'
+
+export default {
+  input: './manifest.json',
+  output: {
+    dir: 'dist',
+    format: 'esm'
+  },
+  plugins: [
+    chromeExtension(),
+    pushReloader(),
+    resolve(),
+    commonjs()
+  ]
+}
